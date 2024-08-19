74a7c8c584d494cb5ffc0826373890087c4aba33
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-19 16:58:33 +0300

    basic javascriot

---
+++ JS курс 2024 (видео на ютубе)/typeof.js
@@ -0,0 +1,19 @@
+// ТИПЫ ДАННЫЧ
+
+
+// примитивные типы данных
+const name = 'Bob' // string
+const age = 28 // number
+const isDeveloper = true // boolean
+const power = 99999999999n //bigint
+const sign = Symbol()
+const something = undefined
+
+
+// специальные типы данные 
+const user = {
+  login: '123',
+  password: 'qwerty'
+}
+
+const data = null
