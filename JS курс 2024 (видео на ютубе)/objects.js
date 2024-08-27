f3b5aaae3af7e5bc87285b4d7242248845d06e2a
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-08-27 20:27:56 +0300

    objects part 2

--- JS курс 2024 (видео на ютубе)/objects.js
+++ JS курс 2024 (видео на ютубе)/objects.js
@@ -32,8 +32,83 @@ for (const key in user){
   console.log(key);
 }
 
+// объект ссылочный тип данных, у каждоого объекта уникальный ссылка для объекта памяти
+
+// const obj1 = {name: 'Bob'}
+// const obj2 = obj1
+
+// obj2.name = 'Aser'
+
+// console.log(obj1); // aser, перезаписали
+
+
+//сравниваем два объекта
+const obj1 = {
+  name: 'Bob',
+  age: 28,
+}
+
+const obj2 = {
+  name: 'Bob',
+  age: 28,
+}
+
+const areObjectEqual = (object1, object2) => {
+  const keys1 = Object.keys(object1) //это метод в JS, который возвращает массив строк, представляющих имена (ключи) всех перечисляемых свойств объекта
+  const keys2 = Object.keys(object2)
+
+  console.log('Длина keys1:', keys1.length); //длина объекта
+  console.log('Длина keys2:', keys2.length);
+  
+  if (keys1.length !== keys1.length) { // если наши объекты не равны
+    return false
+  }
+
+  for (const key in object1){ // перебираем свойства в объекта
+    if (object1[key] !== object2[key]){ // если свойсва не одинаковы
+      return false
+    }
+  }
+
+  return true // и если прошли все проверки
+}
+
+console.log(
+  'Равны ли объекты obj1 и obj2:' ,
+  areObjectEqual(obj1, obj2)
+);
 
 
 
 
+// клонирование объекта
+ const objects1 = {name: 'Bob'}
+ const oblects2 = {}
 
+ for(const key in objects1){
+  oblects2[key] = objects1[key]
+ }
+
+// можно записать это следующим образом
+
+const objects2 = Object.assign({}, objects1) // метод используется для копирования значений всех перечисляемых свойств из одного объекта в другой объект (можно несколько объектов передавать)
+const oblc2  = {... objects1} // для копированич объектов, оператор расширения
+
+oblects2.name = 'Aser'
+
+console.log(objects1);
+console.log(oblects2);
+
+
+
+// объединенин объектов
+
+const firstOb = {name: 'Bob'}
+const secondOb = {age: 23}
+
+
+const userTogether = Object.assign({}, firstOb, secondOb) // с помощью этого метода также может объединить
+const usersTogether = {...firstOb, ...secondOb} // также для объединения может использовать этот оператор
+
+
+console.log('user:' , userTogether);
