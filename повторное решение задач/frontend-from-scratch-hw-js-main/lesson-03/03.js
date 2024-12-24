d73e86eca649456cf245080af71426c56133cbe7
VeronikaZenchik <veronikazenchik991@gmail.com>
2024-12-24 21:10:05 +0300

    third lesson

--- повторное решение задач/frontend-from-scratch-hw-js-main/lesson-03/03.js
+++ повторное решение задач/frontend-from-scratch-hw-js-main/lesson-03/03.js
@@ -1,3 +1,19 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+if (num1 > num2 && num1 > num3) {
+  return num1
+} else if (num2 > num1 && num2 > num3){
+  return num2
+}
+return num3
+}
+console.log(findLargest(8, 2, 3));
+
+
+// Можно проще
+// function findLargest(num1, num2, num3) {
+//   return Math.max(num1, num2, num3);
+// }
+
+// console.log(findLargest(10, 20, 30));
